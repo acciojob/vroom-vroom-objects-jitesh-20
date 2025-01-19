d58fc2a687b76b59f82551590e4698c8175eab38
Jitesh Kumar <105545377+jitesh-20@users.noreply.github.com>
2025-01-19 15:26:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,15 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	getMakeModel(){
+		return `${this.make} and ${this.model}`
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) extends Car {
+	getTopSpeed(){
+		return `${topSpeed} is the topSpeed of the Car`
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
